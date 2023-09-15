9d60a8b62f8a88f49df85c8c67a38cd8bc424cb4
MrIgDem <analizatop69@gmail.com>
2023-09-15 11:37:44 +0300

    AddAll

---
+++ Задача 41/Program.cs
@@ -0,0 +1,23 @@
+﻿// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Введите количество чисел: ");
+//         int m = int.Parse(Console.ReadLine());
+
+//         int countPositive = 0;
+
+//         for (int i = 0; i < m; i++)
+//         {
+//             Console.Write($"Введите число {i + 1}: ");
+//             int number = int.Parse(Console.ReadLine());
+
+//             if (number > 0)
+//             {
+//                 countPositive++;
+//             }
+//         }
+
+//         Console.WriteLine($"Количество положительных чисел: {countPositive}");
+//     }
+// }
